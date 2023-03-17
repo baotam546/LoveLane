fca6b02525a31cccfdbcf3b952f40532ee59a3bc
baotam546 <donguyenbaotam546@gmail.com>
2023-03-17 15:02:46 +0700

    logOut servlet

--- PRJ-Love Lane/src/java/DAO/userAccountDAO.java
+++ PRJ-Love Lane/src/java/DAO/userAccountDAO.java
@@ -172,9 +172,5 @@ public class userAccountDAO {
         return null;
     }
     
-    public static void main(String[] args) {
-            Connection conn = DBUtils.getConnection();
-            System.out.println(conn==null);
-            
-    }
+   
 }
